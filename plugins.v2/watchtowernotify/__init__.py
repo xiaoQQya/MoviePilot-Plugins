81bb86cdead0eb79eff2c0e552a65f141d4e12e8
xiaoQQya <xiaoQQya@126.com>
2025-11-11 21:04:41 +0800

    perf(watchtowernotify): 修改通知标题

--- plugins.v2/watchtowernotify/__init__.py
+++ plugins.v2/watchtowernotify/__init__.py
@@ -15,7 +15,7 @@ class WatchtowerNotify(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/xiaoQQya/MoviePilot-Plugins/refs/heads/main/icons/watchtower.png"
     # 插件版本
-    plugin_version = "1.0.0"
+    plugin_version = "1.0.1"
     # 插件作者
     plugin_author = "xiaoQQya"
     # 作者主页
@@ -77,7 +77,7 @@ class WatchtowerNotify(_PluginBase):
             if self._msgtype:
                 mtype = NotificationType.__getitem__(str(self._msgtype)) or NotificationType.Manual
             if text:
-                self.post_message(title="【Watchtower通知】",
+                self.post_message(title="🚀 Watchtower 通知",
                                   mtype=mtype,
                                   text=text)
                 logger.info(f"推送 Watchtower 通知成功：\n{text}")
